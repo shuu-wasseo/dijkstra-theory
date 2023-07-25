8f78c937206514e9a02ab1907ae5919c245582e2
shuu-wasseo <joshuasoh2008@gmail.com>
2023-07-25 14:02:05 +0800

    .

--- main.py
+++ main.py
@@ -3,16 +3,30 @@ with open('input.txt') as f:
 
 dic = {}
 
-def pdic(dic):
+units = {
+    "aaa": ["S2", "S5", "S7", "S8"],
+    "assemble": [("S"+str(x+1)) for x in range(10)],
+    "kre": ["S1", "S3", "S4", "S6"],
+    "love": ["S1", "S2", "S8", "S9", "S10", "S13", "S14", "S15"],
+    "evol": [("S"+str(x)) for x in range(3, 8)] + ["S11", "S12", "S16"],
+    "full": ["S"+str(x+1) for x in range(24)]
+}
+
+def pdic(dic, unit):
+    print(unit)
     for x in dic:
-        for y in dic[x]:
-            print(x, y, round(1/dic[x][y], 3))
+        if x in units[unit]:
+            for y in dic[x]:
+                if y in units[unit]:
+                    print(x, y, round(1/dic[x][y], 3))
+    print("\n")
 
 for l in lines:
-    if l == "\n":
-        pdic(dic)
+    l = l.strip("\n")
+    if "-" not in l and l[0] != "S":
+        pdic(dic, l)
     else:
-        l = l.strip("\n").split("-")
+        l = l.split("-")
         for s in range(len(l)-1):
             try:
                 dic[l[s]][l[s+1]] += 1
@@ -21,3 +35,5 @@ for l in lines:
                     dic[l[s]][l[s+1]] = 1
                 except:
                     dic[l[s]] = {l[s+1]: 1}
+
+pdic(dic, "full")
